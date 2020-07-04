c2219e9edaca37386f0eba4335722fabf8bd4c8f
Egor Bedov <bedovegor2203@gmail.com>
2020-07-04 12:28:34 +0300

    Moved store to its own file

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './main.scss';
 
+import store from "./store/store";
 import HomePage from 'Pages/Home/render';
-import DictysPage from 'Pages/Dictys/render';
+import DemoPage from 'Pages/Dictys/render';
 import AboutPage from 'Pages/About/render';
 import SetupsPage from 'Pages/Setups/render';
-import reducer from 'Reducers/MainReducer';
-
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 render(
     <Provider store={store}>
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={HomePage}/>
-                <Route path="/demo" component={DictysPage}/>
+                <Route path="/demo" component={DemoPage}/>
                 <Route path="/setups" component={SetupsPage}/>
                 <Route path="/contribute" component={AboutPage}/>
             </Switch>
