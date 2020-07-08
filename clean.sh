ce01b1f86a3bc236102ec1bea70148ff14f7f887
Egor Bedov <bedovegor2203@gmail.com>
2020-07-08 23:53:26 +0300

    Modify clean.sh

--- clean.sh
+++ clean.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
 
-# Delete static files
-rm -rf static/icons
-rm -rf static/images
+print_usage() {
+	echo "usage: ./clean.sh [-ah] [-N name]
+
+	a : additionally delete components, models, pages, etc
+	h : show help
+	n name: new name for project"
+}
+
+MODE="patrial"
+NAME=""
+
+while getopts "ahn:" flag; do
+	case "${flag}" in
+		a) MODE="full" ;;
+		h) print_usage
+		   exit 0 ;;
+		n) NAME="${OPTARG}" ;;
+		*) print_usage
+		   exit 1 ;;
+	esac
+done
+
+if [[ "$NAME" == "" ]]; then
+	echo "Either you forgot to name your project or entered empty name
+use -h option to get help"
+	exit 1;
+fi
+
+###################################
+######    Actual cleaning    ######
+###################################
+
+rm -rf src/pages
+rm -rf src/components
+if [[ "$MODE" == "full" ]]; then
+  rm -rf static
+  rm -rf src/models
+else
+  rm -rf static/icons
+  rm -rf static/images
+  rm src/models/PersonModel.ts
+  mkdir src/pages
+  mkdir src/components
+
+  sed '18,20d;23d' webpack.config.js
+  sed '12,14d;17d' webpack.config.js
+fi
 
 echo '' > src/main.scss
-echo "import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import './main.scss';
-
-import store from './store/store';
-
-render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-
-            </Switch>
-        </BrowserRouter>
-    </Provider>,
-    document.querySelector('#application')
-);" > src/main.jsx
+sed '8,11d;17,20d' src/main.jsx
 
 # Clean Utils
 echo '' > src/utils/constants.ts
@@ -30,31 +57,14 @@ echo '' > src/utils/interfaces.ts
 echo '' > src/utils/variables.scss
 
 # Clean store
-echo "import { combineReducers } from 'redux';
-
-const reducer = combineReducers({
-
-});
-
-export default reducer;" > src/store/reducers/MainReducer.ts
+sed '5d' src/store/reducers/MainReducer.ts
 rm src/store/reducers/Person.ts
 
 echo "" > src/store/actions/ActionTypes.ts
 rm src/store/actions/Person.ts
 
-# Delete pages
-rm -rf src/pages
-mkdir src/pages
-
-# Delete models
-rm src/models/PersonModel.ts
-
-# Delete components
-rm -rf src/components
-mkdir src/components
-
 # Clean README
-echo "" > README.md
+echo "#$NAME" > README.md
 
 # Delete LICENSE
 rm LICENSE
