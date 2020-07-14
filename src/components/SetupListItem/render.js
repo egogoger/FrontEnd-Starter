d7eb3064016107f8fbb1f11223d479dcfea1b140
Egor Bedov <bedovegor2203@gmail.com>
2020-07-14 23:52:26 +0300

    Static data storage improvements

--- src/components/SetupListItem/render.js
+++ src/components/SetupListItem/render.js
@@ -1,10 +1,6 @@
 import './style.scss';
 
-import {CONST} from 'Constants';
-import JSIcon from 'Static/icons/js.svg';
-import TSIcon from 'Static/icons/ts.svg';
-import ReactIcon from 'Static/icons/react.svg';
-import ReduxIcon from 'Static/icons/redux.svg';
+import Data from 'Static/data';
 
 function SetupsListItem(setup, index) {
     return `
@@ -12,22 +8,7 @@ function SetupsListItem(setup, index) {
     <h2 class='setups-list-item__header'>Setup ${index}</h2>
     <ul class='setups-list-item__stack'>
         ${setup.stack.reduce((output, tech) => {
-        let icon = null;
-        switch (tech) {
-        case CONST.TECH.JS:
-            icon = JSIcon;
-            break;
-        case CONST.TECH.TS:
-            icon = TSIcon;
-            break;
-        case CONST.TECH.REACT:
-            icon = ReactIcon;
-            break;
-        case CONST.TECH.REDUX:
-            icon = ReduxIcon;
-            break;
-        }
-        return `<li><img src=${icon} alt=${tech} />${tech}</li>` + output;
+        return `<li><img src=${Data.Icons.get(tech)} alt=${tech} />${tech}</li>` + output;
     }, '')}
     </ul>
     <a class='setups-list-item__link' href=${setup.link} target='_blank' rel='noopener noreferrer'>Branch &#8594;</a>
